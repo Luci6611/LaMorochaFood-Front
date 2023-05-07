06e282f9461cd04fde9238a3721a232627956272
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-05-07 01:50:15 -0300

    se crea la estructura

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className='w-100 bg-dark  position-absolute bottom-0'>
+        <div>
+vvvvv
+        </div>
+    </footer>
   )
 }
 
--- src/components/NavBars.jsx
+++ src/components/NavBars.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const NavBars = () => {
   return (
-    <nav>NavBars</nav>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Hamburgueseria La Morocha</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">Contacto</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
