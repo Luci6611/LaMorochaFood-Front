84e9e2e9c18f0abe959df6146b676b73d6058c29
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-05-10 01:47:00 -0300

    se prepara firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCknV1dQIh05tyMPH_MjF8HkhZNrZFU6U",
+  authDomain: "la-morocha-9466d.firebaseapp.com",
+  projectId: "la-morocha-9466d",
+  storageBucket: "la-morocha-9466d.appspot.com",
+  messagingSenderId: "693092020897",
+  appId: "1:693092020897:web:42a380f3d751dcd4dff6e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
