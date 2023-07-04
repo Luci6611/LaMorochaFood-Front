7792a49a1574e2326fc9ef563ffa8f0235814576
luciano rivera <lucianonicolasrivera8@gmail.com>
2023-07-04 14:08:29 -0300

    terminamos sobrenosotros falta responsive

--- src/pages/conocenos.jsx
+++ src/pages/conocenos.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
+import Image from 'next/image'
 
 const conocenos = () => {
   return (
     <div className='body-sobre-nosotros'>
 <div className="contenedor-nosotros">
-  <h1>Conoce a La morocha</h1>
+  <h1>La Morocha</h1>
+  <hr className='hrsobrenosotros' />
   <div className="cont-img-sobrenosotros">
- <img src="../img/lamorochalogo.jpg" alt="logo" />
+
+  <img src="/lamorochalogo2.jpg" alt="lamorochalogo" />
+  </div>
+  <h2>como surgio?</h2>
+  <div className="sobrenosotrosuno">
+    <div className="hamburguesauno-nosotros">
+    <img src="/fondo3.jpg" alt="hamburguesa" /></div>
+    <div className="textouno">
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero, consectetur nostrum? Distinctio dolor illo assumenda saepe alias quae exercitationem. Odit ipsum, earum eum expedita dolores quisquam ducimus tenetur aliquid ex.</p></div>
+  </div>
+  <div className="sobrenosotrosdos">
+    <div className="textodos">
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero, consectetur nostrum? Distinctio dolor illo assumenda saepe alias quae exercitationem. Odit ipsum, earum eum expedita dolores quisquam ducimus tenetur aliquid ex.</p></div>
+    <div className="hamburguesdos-nosotros">
+    <img src="/fondo5.jpg" alt="hamburguesa" /></div>
+  </div>
+  <h2>Nuestro equipo</h2>
+  <div className="equipocontenedor">
+    <img src="/personauno.jpg" alt="" />
+    <img src="/personauno.jpg" alt="" />
+    <img src="/personauno.jpg" alt="" />
   </div>
 </div>
 
