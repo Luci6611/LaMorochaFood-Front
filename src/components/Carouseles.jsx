f7308a752c14dfaae024d12ed8fe74a0b1ccdd6c
luciano rivera <lucianonicolasrivera8@gmail.com>
2023-07-04 17:02:04 -0300

    cambiamos fotos de carrousel

--- src/components/Carouseles.jsx
+++ src/components/Carouseles.jsx
@@ -9,21 +9,21 @@ const Carouseles = () => {
             <Carousel.Item >
                 <img
                     className="d-block w-100 h-10 carrimg"
-                    src="https://images2.alphacoders.com/560/560491.jpg"
+                    src="https://images7.alphacoders.com/128/1286471.jpg"
                     alt="First slide"
                 />
             </Carousel.Item>
             <Carousel.Item >
                 <img
                     className="d-block w-100 h-25  carrimg"
-                    src="https://images3.alphacoders.com/276/276759.jpg"
+                    src="https://images6.alphacoders.com/908/908160.jpg"
                     alt="Second slide"
                 />
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100 carrimg"
-                    src="https://images6.alphacoders.com/300/300228.jpg"
+                    src="https://images6.alphacoders.com/870/870777.jpg"
                     alt="Third slide"
                 />
             </Carousel.Item>
