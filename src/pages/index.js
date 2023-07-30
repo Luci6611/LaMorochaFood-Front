2138aec6e51cadbedf6eff75a7111fd07ff086c1
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-07-30 19:31:31 -0300

    se soluciona error de carga ilimitada

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,9 @@ export default function Home() {
     <>
       <Head>
         <title>La Morocha - Home</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />    
         <meta name="description" content="Bienvenidos al sitio Web de La Morocha!. Aqui podras conocer nuestros menues y pedir el tuyo!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
         <link rel="styles-sheets" href="../stylescomponents.css" />
       </Head>   
       <Carouseles/>
