273e73e16cc009bff3a1bafd3611495f5d9fece1
luciano rivera <lucianonicolasrivera8@gmail.com>
2023-08-01 17:04:34 -0300

    arreglamos error admin

--- src/pages/admi.jsx
+++ src/pages/admi.jsx
@@ -50,7 +50,7 @@ const admi = () => {
       {
         token ?
 
-            <main className='container  p-3 '>
+            <main className='contenedoradmin  p-3 '>
               {edit ?
                 <TablaEditarMenus />
                 :
